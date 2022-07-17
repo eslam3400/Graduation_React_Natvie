512795ca0f4abfaca06ca9b99ed804f965b279f5
Eslam Magdy <eslam3400@gmail.com>
2022-07-17 03:17:42 +0200

    intro page

--- screens/intro/Intro.js
+++ screens/intro/Intro.js
@@ -1,20 +1,23 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { Layout, Text, Button } from '@ui-kitten/components'
+import { Feather } from '@expo/vector-icons';
+import MyStyles from '../../Styles'
 
-function Intro() {
+function Intro({ navigation }) {
   return (
-    <View style={style.container}>
-      <Text>Intro Page</Text>
-    </View>
+    <Layout style={MyStyles.container}>
+      <Layout style={{ height: "70%", justifyContent: "center" }}>
+        <Text category='h4'>Welcome To</Text>
+        <Text style={{ fontSize: 55, marginVertical: 10 }} status="primary">
+          <Feather name="shield" size={50} color="#3366FF" /> Live Guard
+        </Text>
+        <Text style={{ textAlign: "center" }}>Some bla bla about us :D</Text>
+      </Layout>
+      <Button style={{ marginTop: 20, width: '80%' }} size="large" onPress={() => navigation.navigate("Login")} >Login</Button>
+      <Button style={{ marginTop: 10, width: '80%' }} size="large" appearance="outline" onPress={() => navigation.navigate("Signup")}>Signup</Button>
+    </Layout >
   )
 }
 
-const style = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-  }
-})
 
 export default Intro
